1a67966bc61a7b85e96e46a678ebc337234c68eb
agustinalejo1 <agustinalejoechaniz@gmail.com>
2024-06-12 20:33:42 -0300

    Revert "Add files via upload"
    This reverts commit 86310533d609f358b5fe4d2120d2ef11d2d0354f.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-//el nodemon es una herramiento que permtie hacer cambios sin tener que parar el server y volverlo a prender. supongo que lo hace solo o algo. por eso usamos nodemon y no node cuando hacen el dev en package.json
-const express = require('express'); // "el ; es por buena practica jsjsaj" esto trae el modulo express
+const express = require('express')
+const port = process.env.port ?? 3000
+const rutas=require("./rutas/carreras.ruta")
+const app = express()
 
-const rutas = require("./rutas") //esto me trae todo lo que este en el module.exports en el index.js de la carpeta rutas (usa el inted pero no le especifico que carpeta y esta es la predetermianda)
+app.use(express.json());
+app.use(rutas)
 
-const app=express(); // esto ejecuta el xpress que trajimos "se le suele poner app o aplication o server".... todos los requires (importaciones) van al comienzo de todo, por buena praxis
-app.use(express.json()); //esto le dice que todo lo que mande lo trate como un json
-
-app.use(rutas.seriesRutas) //le estoy diciendo que lo la exportacion de rutas que hice en la linea 3, use el atributo que le digo despues del punto
-app.use(rutas.actoresRutas)
-app.use(rutas.usuariosRutas)
-
-app.listen(777,() => {
-    console.log("servidor corriendo jerfdjk")
+app.listen(port, ()=>{
+    console.log(`server corriendo con el puerto ${port}`)
 });
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
